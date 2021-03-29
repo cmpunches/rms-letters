54c37f4d25707ec6d9f7affca1ca11d6a84b9b89
SpheeresX <76824533+SpheeresX@users.noreply.github.com>
2021-03-29 17:33:25 +0200

    show pending (unmerged) signatures

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ app.get('/api/stats', cache('1 minute'), async (req, res) => {
     // fetch the pages and convert the response to plain text
     const OL = await fetch('https://rms-open-letter.github.io/');
     const SL = await fetch('https://rms-support-letter.github.io/');
+    const SL_PRDATA = await fetch('https://api.github.com/repos/rms-support-letter/rms-support-letter.github.io/pulls?state=open');
     const OL_TEXT = await OL.text();
     const SL_TEXT = await SL.text();
+    const SL_PRDATA_JSON = await SL_PRDATA.json();
     // parse the data with cheerio 
     const $OL = cheerio.load(OL_TEXT);
     const $SL = cheerio.load(SL_TEXT);
@@ -21,7 +23,7 @@ app.get('/api/stats', cache('1 minute'), async (req, res) => {
     const OL_NUM = $OL('ol').children('li').length;
     const SL_NUM = $SL('ol').children('li').length;
 
-    res.json({ open: OL_NUM, support: SL_NUM });
+    res.json({ open: OL_NUM, support: SL_NUM, supportPending: SL_PRDATA_JSON.length });
 });
 
 app.listen(process.env.PORT || 3000);
